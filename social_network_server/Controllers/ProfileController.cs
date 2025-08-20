6ea9c3dd25aecc26c0c1b005d8605d6cfbc225b5
DuccDung <duccdung999@gmail.com>
2025-08-20 17:03:53 +0700

    add auth

---
+++ social_network_server/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using social_network_server.Models;
+
+namespace social_network_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly SocialNetworkContext _context;
+        public ProfileController(SocialNetworkContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("/api/get_profile/{userId}")]
+        public async Task<IActionResult> GetProfile(int userId)
+        {
+            var profile = await _context.Profiles
+                .FirstOrDefaultAsync(p => p.UserId == userId);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+            return Ok(profile);
+        }
+
+        [HttpPost("/api/create_profile")]
+        public async Task<IActionResult> CreateProfile([FromBody] Profile profile)
+        {
+            if (profile == null)
+            {
+                return BadRequest("Profile data is required.");
+            }
+            await _context.Profiles.AddAsync(profile);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProfile), new { userId = profile.UserId }, profile);
+        }
+    }
+}
---
+++ social_network_server/Models/ConversationMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace social_network_server.Models;
+
+public partial class ConversationMember
+{
+    public int ConversationId { get; set; }
+
+    public int AccountId { get; set; }
+
+    public DateTime? JoinedAt { get; set; }
+
+    public string? Title { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Account Account { get; set; } = null!;
+
+    public virtual Conversation Conversation { get; set; } = null!;
+}
