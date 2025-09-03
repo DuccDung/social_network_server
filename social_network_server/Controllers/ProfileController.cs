47ce2cac3b7d4a863418957d5f371395f8071f77
DuccDung <duccdung999@gmail.com>
2025-09-03 12:08:19 +0700

    fix create profile

--- social_network_server/Controllers/ProfileController.cs
+++ social_network_server/Controllers/ProfileController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using social_network_server.Models;
+using social_network_server.Models.ModelBase;
 
 namespace social_network_server.Controllers
 {
@@ -27,13 +28,34 @@ namespace social_network_server.Controllers
         }
 
         [HttpPost("/api/create_profile")]
-        public async Task<IActionResult> CreateProfile([FromBody] Profile profile)
+        public async Task<IActionResult> CreateProfile([FromBody] profile_req profile)
         {
             if (profile == null)
             {
                 return BadRequest("Profile data is required.");
             }
-            await _context.Profiles.AddAsync(profile);
+
+            var existingProfile = await _context.Profiles
+                .FirstOrDefaultAsync(p => p.UserId == profile.UserId);
+            if (existingProfile != null)
+            {
+                return Ok(existingProfile);
+            }
+            Profile newProfile = new Profile
+            {
+                UserId = profile.UserId,
+                FullName = profile.FullName,
+                Address = profile.Address,
+                AvatarUrl = profile.AvatarUrl,
+                Bio = profile.Bio,
+                CreateAt = DateTime.Now,
+                UpdateAt = DateTime.Now,
+                DateOfBirth = profile.DateOfBirth,
+                Gender = profile.Gender,
+                Phone = profile.Phone
+            };
+
+            await _context.Profiles.AddAsync(newProfile);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetProfile), new { userId = profile.UserId }, profile);
         }
